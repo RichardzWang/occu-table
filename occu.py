593dac5bfde3fefc96aba2c2a2e2267863ccd40d
Richard Wang <richardwang@Richards-MacBook-Pro.local>
2016-09-24 00:34:42 -0400

    Basic HW Done

--- occu.py
+++ occu.py
@@ -2,10 +2,32 @@
 #9/22/16
 #Period 9
 
-import profession.py
-from Flask import flask, render_template
 
-poslist = profession.reader("occupations.csv")
+
+import csv, random
+from flask import Flask, render_template
+
+
+
+def parseCSV(filename):
+    with open(filename, "r") as raw:
+        reader = csv.reader(raw)
+        parsed = dict(reader)
+        del parsed["Job Class"]
+        return parsed
+
+def choose(data):
+    seed = random.random() * 99.8; # seed value
+    for key in data:
+        # print ( seed ) 
+        seed -= float(data[key])
+        if seed < 0:
+            return key
+    return -1
+
+
+
+occulist = parseCSV("occupations.csv")
 
 app = Flask(__name__)
 
@@ -15,8 +37,8 @@ def home():
 
 @app.route("/occupations")
 def occupation():
-    return render_template('occupations.html', posoccupations = poslist,
-occupation = profession.choose(poslist))
+    return render_template('occupations.html', dictionary = occulist, future = choose(occulist))
+
     
 
 if __name__ == '__main__':
